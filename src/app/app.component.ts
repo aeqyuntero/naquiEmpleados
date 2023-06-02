f366ebd5a9c06e29aeb7c835ee1ab89b87d34b05
Esteban Quintero <aeqyuntero@gmail.com>
2023-06-02 10:57:52 -0500

    remover localStorage al iniciar la página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'naquiEmpleados';
+
+  ngOnInit() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('usuario');
+  }
 
   get isVisible() {
     return (
